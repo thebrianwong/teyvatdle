7e2258094503ac4c013124730e9f349604680181
Brian Wong <brianw7218@gmail.com>
2023-08-30 21:31:24 -0700

    Add visual indication of how correct a guess is by adding class styling and refactoring the Cell component

--- src/components/GuessTableCell/GuessTableCell.tsx
+++ src/components/GuessTableCell/GuessTableCell.tsx
@@ -1,40 +1,37 @@
 import { ReactElement } from "react";
 import GuessTableCellProps from "./type";
+import "./styles.scss";
 
 const GuessTableCell = ({ cellData }: GuessTableCellProps) => {
   let cellElement: ReactElement;
 
   if (cellData.dataType === "mainImage") {
-    cellElement = (
-      <td>
-        <img src={cellData.content} alt={cellData.altText} />
-      </td>
-    );
+    cellElement = <img src={cellData.content} alt={cellData.altText} />;
   } else if (cellData.dataType === "textSingle") {
     const cellContent = cellData.content || "None";
-    cellElement = <td>{cellContent}</td>;
+    cellElement = <>{cellContent}</>;
   } else if (cellData.dataType === "textDouble") {
     const cellContent1 = cellData.content1 || "None";
     const cellContent2 = cellData.content2 || "None";
     cellElement = (
-      <td>
+      <>
         {cellContent1}
         <br />
         {cellContent2}
-      </td>
+      </>
     );
   } else if (cellData.dataType === "textImageCombo") {
     const cellContent1 = cellData.content1 || "None";
     cellElement = (
-      <td>
+      <>
         {cellContent1}
         <img src={cellData.content2} alt={cellData.altText2 + "."} />
-      </td>
+      </>
     );
   } else if (cellData.dataType === "imageDouble") {
     if (Array.isArray(cellData.content1) && Array.isArray(cellData.altText1)) {
       cellElement = (
-        <td>
+        <>
           {cellData.content1.map((content, index) => {
             return (
               <img
@@ -45,7 +42,7 @@ const GuessTableCell = ({ cellData }: GuessTableCellProps) => {
             );
           })}
           <img src={cellData.content2!} alt={cellData.altText2 + "."} />
-        </td>
+        </>
       );
     } else if (
       !Array.isArray(cellData.content1) &&
@@ -53,33 +50,33 @@ const GuessTableCell = ({ cellData }: GuessTableCellProps) => {
     ) {
       if (cellData.content1 === null) {
         cellElement = (
-          <td>
+          <>
             None
             <img src={cellData.content2!} alt={cellData.altText2 + "."} />
-          </td>
+          </>
         );
       } else if (cellData.content2 === null) {
         cellElement = (
-          <td>
+          <>
             <img src={cellData.content1} alt={cellData.altText1 + "."} />
             None
-          </td>
+          </>
         );
       } else {
         cellElement = (
-          <td>
+          <>
             <img src={cellData.content1} alt={cellData.altText1 + "."} />
             <img src={cellData.content2} alt={cellData.altText2 + "."} />
-          </td>
+          </>
         );
       }
     }
   } else if (cellData.dataType === "booleanSingle") {
     const cellContent = cellData.content ? "✅" : "❌";
-    cellElement = <td>{cellContent}</td>;
+    cellElement = <>{cellContent}</>;
   }
 
-  return cellElement!;
+  return <td className={`${cellData.answerAccuracy}`}>{cellElement!}</td>;
 };
 
 export default GuessTableCell;
