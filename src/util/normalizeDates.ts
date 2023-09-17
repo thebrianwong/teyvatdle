e32d32ecb48b099476a3501e490b4cff2f424b35
Brian Wong <brianw7218@gmail.com>
2023-09-16 21:57:40 -0700

    Add utility function to get date in Pacific time

---
+++ src/util/normalizeDates.ts
@@ -0,0 +1,40 @@
+const normalizeYear: () => string = () => {
+  const year = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+  )
+    .getFullYear()
+    .toString();
+  return year;
+};
+
+const normalizeMonth: () => string = () => {
+  const rawMonth =
+    new Date(
+      new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+    ).getMonth() + 1;
+  if (rawMonth < 10) {
+    const formattedMonth = "0".concat(String(rawMonth));
+    return formattedMonth;
+  } else {
+    return rawMonth.toString();
+  }
+};
+
+const normalizeDay: () => string = () => {
+  const rawDay = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })
+  ).getDate();
+  if (rawDay < 10) {
+    const formattedDay = "0".concat(String(rawDay));
+    return formattedDay;
+  } else {
+    return rawDay.toString();
+  }
+};
+
+const getNormalizeDate: () => string = () => {
+  const dateString = `${normalizeYear()}-${normalizeMonth()}-${normalizeDay()}`;
+  return dateString;
+};
+
+export default getNormalizeDate;
