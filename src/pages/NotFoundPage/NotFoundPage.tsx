377127cd02ec9263bf2899ef3f8f6929e072fa23
Brian Wong <brianw7218@gmail.com>
2023-09-24 22:29:29 -0700

    Style the 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -3,9 +3,16 @@ import kokomiOfGreed from "../../assets/misc/kokomiOfGreed.png";
 const NotFoundPage = () => {
   return (
     <>
-      <h1>404</h1>
-      <h2>How about we explore the area ahead of us later?</h2>
+      <header style={{ flexDirection: "column" }}>
+        <h1>404</h1>
+        <h2>How about we explore the area ahead of us later?</h2>
+      </header>
       <img
+        style={{
+          borderRadius: "300px",
+          boxShadow: "0 0 20px 1px #f5f5f5",
+          width: "clamp(300px, 100% ,800px)",
+        }}
         src={kokomiOfGreed}
         alt="A rendition of the Yu-Gi-Oh! card Pot of Greed styled after Kokomi."
       />
