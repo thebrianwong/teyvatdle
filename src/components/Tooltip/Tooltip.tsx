d068152fc2e6224239ff0ae7b7542f184d840337
Brian Wong <brianw7218@gmail.com>
2023-09-26 22:26:32 -0700

    Further style the tooltip and add logic to display when the button is clicked

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,41 +1,76 @@
+import { useEffect, useRef, useState } from "react";
 import "./styles.scss";
 
 const Tooltip = () => {
+  const [isDisplaying, setIsDisplaying] = useState(false);
+  const boxRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        isDisplaying &&
+        boxRef.current &&
+        !boxRef.current.contains(e.target as HTMLElement) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(e.target as HTMLElement)
+      ) {
+        setIsDisplaying(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isDisplaying]);
+
   return (
     <div
       style={{
         position: "fixed",
-        bottom: "50px",
-        right: "50px",
+        bottom: "30px",
+        right: "15px",
         display: "flex",
         flexDirection: "column",
-        gap: "16px",
+        gap: "24px",
+        maxWidth: "290px",
       }}
     >
-      <div className="arrow-box">
-        <p>
-          <span style={{ color: "#e03c31" }}>Red</span>: Both answers are wrong.
-        </p>
-        <p style={{ marginLeft: "16px" }}>Birthday: Month and day are wrong.</p>
-        <p>
-          <span style={{ color: "#fdee00" }}>Yellow</span>: One answer correct.
-        </p>
-        <p style={{ marginLeft: "16px" }}>
-          Birthday: Either month or day are correct.
-        </p>
-        <p>
-          <span style={{ color: "#00a86b" }}>Green</span>: Both answers are
-          correct.
-        </p>
-      </div>
+      {isDisplaying && (
+        <div className="arrow-box" ref={boxRef}>
+          <p>
+            <span style={{ color: "#e03c31" }}>Red</span>: Both answers are
+            wrong.
+          </p>
+          <p style={{ marginLeft: "16px" }}>
+            Birthday: Month and day are wrong.
+          </p>
+          <p>
+            <span style={{ color: "#fdee00" }}>Yellow</span>: One answer is
+            correct.
+          </p>
+          <p style={{ marginLeft: "16px" }}>
+            Birthday: Either month or day are correct.
+          </p>
+          <p>
+            <span style={{ color: "#00a86b" }}>Green</span>: Both answers are
+            correct.
+          </p>
+        </div>
+      )}
       <button
         className="tooltip-button"
         style={{
           borderRadius: "20px",
-          backgroundColor: "lightgray",
-          border: "solid 4px darkgray",
+          position: "relative",
+          border: "4px solid darkgray",
+          right: "19.5px",
           alignSelf: "end",
         }}
+        onClick={() => setIsDisplaying(!isDisplaying)}
+        ref={buttonRef}
       >
         ?
       </button>
