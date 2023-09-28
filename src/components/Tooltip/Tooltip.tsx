032b41be837bcf2efdd12d70018ff96c504c2a6d
Brian Wong <brianw7218@gmail.com>
2023-09-27 22:11:59 -0700

    Move tooltip styling from inline to stylesheet

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -28,54 +28,34 @@ const Tooltip = ({ type }: TooltipProps) => {
   }, [isDisplaying]);
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: "30px",
-        right: "15px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "24px",
-        maxWidth: "290px",
-      }}
-    >
+    <div className="tooltip-container">
       {isDisplaying && (
         <div className="arrow-box" ref={boxRef}>
           <p>
-            <span style={{ color: "#e03c31" }}>Red</span>: Both answers are
-            wrong.
+            <span className="tooltip-wrong">Red</span>: Both answers are wrong.
           </p>
           {type === "character" && (
-            <p style={{ marginLeft: "16px" }}>
+            <p className="tooltip-birthday">
               Birthday: Month and day are wrong.
             </p>
           )}
           <p>
-            <span style={{ color: "#fdee00" }}>Yellow</span>: One answer is
+            <span className="tooltip-partial">Yellow</span>: One answer is
             correct.
           </p>
           {type === "character" && (
-            <p style={{ marginLeft: "16px" }}>
+            <p className="tooltip-birthday">
               Birthday: Either month or day are correct.
             </p>
           )}
           <p>
-            <span style={{ color: "#00a86b" }}>Green</span>: Both answers are
+            <span className="tooltip-correct">Green</span>: Both answers are
             correct.
           </p>
         </div>
       )}
       <button
         className="tooltip-button"
-        style={{
-          borderRadius: "30px",
-          position: "relative",
-          border: "4px solid darkgray",
-          right: "19.5px",
-          alignSelf: "end",
-          width: "50px",
-          height: "50px",
-        }}
         onClick={() => setIsDisplaying(!isDisplaying)}
         ref={buttonRef}
       >
