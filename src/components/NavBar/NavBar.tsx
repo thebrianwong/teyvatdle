1818073924d4f91b88cefcf791b2026d332d02b2
Brian Wong <brianw7218@gmail.com>
2023-09-30 23:16:03 -0700

    Start to make the nav bar have a button when the viewport lacks space to display all nav items

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,8 +6,29 @@ import foodIcon from "../../assets/nav/navFood.png";
 import talentIcon from "../../assets/nav/navTalent.png";
 import constellationIcon from "../../assets/nav/navConstellation.png";
 import "./styles.scss";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [isButtonMenu, setIsButtonMenu] = useState(
+    window.innerWidth <= 1024 ? true : false
+  );
+
+  useEffect(() => {
+    const listenForResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", listenForResize);
+
+    return () => {
+      window.removeEventListener("resize", listenForResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.innerWidth <= 1024 ? setIsButtonMenu(true) : setIsButtonMenu(false);
+  }, [width]);
+
   return (
     <nav className="nav-bar">
       <Link to="/">
