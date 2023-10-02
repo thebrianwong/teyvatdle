1b1c890510b95b8a53bbdd0d92758035078f35c6
Brian Wong <brianw7218@gmail.com>
2023-10-01 21:56:03 -0700

    Replace the nav bar content with a hamburger menu on smaller viewports

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/nav/logo.png";
-import characterIcon from "../../assets/nav/navCharacter.png";
-import weaponIcon from "../../assets/nav/navWeapon.png";
-import foodIcon from "../../assets/nav/navFood.png";
-import talentIcon from "../../assets/nav/navTalent.png";
-import constellationIcon from "../../assets/nav/navConstellation.png";
 import "./styles.scss";
 import { useEffect, useState } from "react";
+import NavBarLinks from "./NavBarLinks";
 
 const NavBar = () => {
+  const location = useLocation();
   const [width, setWidth] = useState(window.innerWidth);
   const [isButtonMenu, setIsButtonMenu] = useState(
-    window.innerWidth <= 1024 ? true : false
+    window.innerWidth <= 570 ? true : false
   );
+  const [displayMenu, setDisplayMenu] = useState(false);
 
   useEffect(() => {
     const listenForResize = () => {
@@ -26,34 +24,46 @@ const NavBar = () => {
   }, []);
 
   useEffect(() => {
-    window.innerWidth <= 1024 ? setIsButtonMenu(true) : setIsButtonMenu(false);
+    window.innerWidth <= 570 ? setIsButtonMenu(true) : setIsButtonMenu(false);
+    setDisplayMenu(false);
   }, [width]);
+
+  useEffect(() => {
+    setDisplayMenu(false);
+  }, [location]);
 
   return (
     <nav className="nav-bar">
       <Link to="/">
         <img className="logo" src={logo} alt="Teyvatdle Logo." />
       </Link>
-      <Link to="/">
-        Characters
-        <img src={characterIcon} alt="" />
-      </Link>
-      <Link to="/weapon">
-        Weapons
-        <img src={weaponIcon} alt="" />
-      </Link>
-      <Link to="/food">
-        Foods
-        <img src={foodIcon} alt="" />
-      </Link>
-      <Link to="/talent">
-        Talents
-        <img src={talentIcon} alt="" />
-      </Link>
-      <Link to="/constellation">
-        Constellations
-        <img src={constellationIcon} alt="" />
-      </Link>
+      {isButtonMenu ? (
+        <>
+          <button
+            className="menu-button"
+            onClick={() => setDisplayMenu(!displayMenu)}
+          >
+            <svg
+              className="menu-button-image"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <title>menu</title>
+              <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+            </svg>
+          </button>
+          <div
+            style={{
+              display: displayMenu ? "flex" : "none",
+            }}
+            className="responsive-menu-container"
+          >
+            <NavBarLinks />
+          </div>
+        </>
+      ) : (
+        <NavBarLinks />
+      )}
     </nav>
   );
 };
