13cdec2f317d6fea4933c80dce31486d8fe4e57d
Brian Wong <brianw7218@gmail.com>
2023-10-03 21:30:02 -0700

    Adjust the 404 page styling due to previous general page styling changes

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -5,13 +5,17 @@ const NotFoundPage = () => {
     <>
       <header style={{ flexDirection: "column" }}>
         <h1>404</h1>
-        <h2>How about we explore the area ahead of us later?</h2>
+        <h2 style={{ textAlign: "center" }}>
+          How about we explore the area ahead of us later?
+        </h2>
       </header>
       <img
         style={{
           borderRadius: "300px",
           boxShadow: "0 0 20px 1px #f5f5f5",
-          width: "clamp(300px, 100% ,800px)",
+          width: "100%",
+          maxWidth: "800px",
+          margin: "0 auto",
         }}
         src={kokomiOfGreed}
         alt="A rendition of the Yu-Gi-Oh! card Pot of Greed styled after Kokomi."
