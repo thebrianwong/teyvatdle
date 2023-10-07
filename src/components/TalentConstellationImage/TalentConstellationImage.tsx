6964863fb33c8114891763de7661a6c73200d11f
Brian Wong <brianw7218@gmail.com>
2023-10-06 22:30:13 -0700

    Memoize the talent and constellation images so that they don't rerender upon guess

--- src/components/TalentConstellationImage/TalentConstellationImage.tsx
+++ src/components/TalentConstellationImage/TalentConstellationImage.tsx
@@ -1,59 +1,59 @@
-import { ReactElement, useEffect, useRef } from "react";
+import { ReactElement, memo, useEffect, useRef } from "react";
 import TalentAPIData from "../../types/data/talentAPIData.type";
 import TalentConstellationImageProps from "./type";
 import ConstellationAPIData from "../../types/data/constellationAPIData.type";
 
-const TalentConstellationImage = ({
-  type,
-  data,
-}: TalentConstellationImageProps) => {
-  const ref = useRef<HTMLImageElement>(null);
+const TalentConstellationImage = memo(
+  ({ type, data }: TalentConstellationImageProps) => {
+    const ref = useRef<HTMLImageElement>(null);
 
-  let image: ReactElement;
+    let image: ReactElement;
 
-  useEffect(() => {
-    const preventRightClick = (e: MouseEvent) => {
-      e.preventDefault();
-    };
+    useEffect(() => {
+      const preventRightClick = (e: MouseEvent) => {
+        e.preventDefault();
+      };
 
-    ref.current?.addEventListener("contextmenu", preventRightClick);
+      ref.current?.addEventListener("contextmenu", preventRightClick);
 
-    const imageRef = ref.current!;
+      const imageRef = ref.current!;
 
-    return () => imageRef.removeEventListener("contextmenu", preventRightClick);
-  }, []);
+      return () =>
+        imageRef.removeEventListener("contextmenu", preventRightClick);
+    }, []);
 
-  if (type === "talent") {
-    image = (
-      <img
-        ref={ref}
-        src={(data as TalentAPIData).talent_image_url}
-        alt="Daily talent."
-        style={{
-          alignSelf: "center",
-          padding: "16px 0",
-          width: "125px",
-          height: "auto",
-        }}
-      />
-    );
-  } else {
-    image = (
-      <img
-        ref={ref}
-        src={(data as ConstellationAPIData).constellation_image_url}
-        alt="Daily constellation."
-        style={{
-          alignSelf: "center",
-          padding: "16px 0",
-          width: "125px",
-          height: "auto",
-        }}
-      />
-    );
+    if (type === "talent") {
+      image = (
+        <img
+          ref={ref}
+          src={(data as TalentAPIData).talent_image_url}
+          alt="Daily talent."
+          style={{
+            alignSelf: "center",
+            padding: "16px 0",
+            width: "125px",
+            height: "auto",
+          }}
+        />
+      );
+    } else {
+      image = (
+        <img
+          ref={ref}
+          src={(data as ConstellationAPIData).constellation_image_url}
+          alt="Daily constellation."
+          style={{
+            alignSelf: "center",
+            padding: "16px 0",
+            width: "125px",
+            height: "auto",
+          }}
+        />
+      );
+    }
+
+    return image;
   }
-
-  return image;
-};
+);
 
 export default TalentConstellationImage;
