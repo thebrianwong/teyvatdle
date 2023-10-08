65548540f3091afdd971af7cb5c1d8c16d3b3937
Brian Wong <brianw7218@gmail.com>
2023-10-07 23:00:42 -0700

    Get rid of delay where the timer would have no values on initial render

--- src/components/ResetTimer/ResetTimer.tsx
+++ src/components/ResetTimer/ResetTimer.tsx
@@ -2,53 +2,60 @@ import { useEffect, useState } from "react";
 import AnimatedValue from "../AnimatedValue/AnimatedValue";
 
 const ResetTimer = () => {
-  const [hoursTens, setHoursTens] = useState<string>("X");
-  const [hoursOnes, setHoursOnes] = useState<string>("X");
-  const [minutesTens, setMinutesTens] = useState<string>("X");
-  const [minutesOnes, setMinutesOnes] = useState<string>("X");
-  const [secondsTens, setSecondsTens] = useState<string>("X");
-  const [secondsOnes, setSecondsOnes] = useState<string>("X");
+  const [hoursTens, setHoursTens] = useState<string>();
+  const [hoursOnes, setHoursOnes] = useState<string>();
+  const [minutesTens, setMinutesTens] = useState<string>();
+  const [minutesOnes, setMinutesOnes] = useState<string>();
+  const [secondsTens, setSecondsTens] = useState<string>();
+  const [secondsOnes, setSecondsOnes] = useState<string>();
 
-  useEffect(() => {
+  const findTimeToReset = () => {
     const pacificMidnight = new Date(
       new Date().toLocaleString("en", { timeZone: "America/Los_Angeles" })
     );
     pacificMidnight.setHours(24, 0, 0, 0);
 
+    const pacificCurrent = new Date(
+      new Date().toLocaleString("en", { timeZone: "America/Los_Angeles" })
+    );
+    const timeDifference =
+      (pacificMidnight.getTime() - pacificCurrent.getTime()) / 1000;
+    const h = Math.floor(timeDifference / 3600).toString();
+    const m = Math.floor((timeDifference % 3600) / 60).toString();
+    const s = ((timeDifference % 3600) % 60).toString();
+    if (h.length === 2) {
+      setHoursTens(h[0]);
+      setHoursOnes(h[1]);
+    } else {
+      setHoursTens("0");
+      setHoursOnes(h);
+    }
+    if (m.length === 2) {
+      setMinutesTens(m[0]);
+      setMinutesOnes(m[1]);
+    } else {
+      setMinutesTens("0");
+      setMinutesOnes(m);
+    }
+    if (s.length === 2) {
+      setSecondsTens(s[0]);
+      setSecondsOnes(s[1]);
+    } else {
+      setSecondsTens("0");
+      setSecondsOnes(s);
+    }
+
+    if (h === "0" && m === "0" && s === "0") {
+      window.location.reload();
+    }
+  };
+
+  useEffect(() => {
+    // without this, there is a 1 second period where the timer has no values
+    findTimeToReset();
+
     const intervalId = setInterval(() => {
-      const pacificCurrent = new Date(
-        new Date().toLocaleString("en", { timeZone: "America/Los_Angeles" })
-      );
-      const timeDifference =
-        (pacificMidnight.getTime() - pacificCurrent.getTime()) / 1000;
-      const h = Math.floor(timeDifference / 3600).toString();
-      const m = Math.floor((timeDifference % 3600) / 60).toString();
-      const s = ((timeDifference % 3600) % 60).toString();
-      if (h.length === 2) {
-        setHoursTens(h[0]);
-        setHoursOnes(h[1]);
-      } else {
-        setHoursTens("0");
-        setHoursOnes(h);
-      }
-      if (m.length === 2) {
-        setMinutesTens(m[0]);
-        setMinutesOnes(m[1]);
-      } else {
-        setMinutesTens("0");
-        setMinutesOnes(m);
-      }
-      if (s.length === 2) {
-        setSecondsTens(s[0]);
-        setSecondsOnes(s[1]);
-      } else {
-        setSecondsTens("0");
-        setSecondsOnes(s);
-      }
-      // reload the page if a user actually ends up waiting for reset
-      if (h === "0" && m === "0" && s === "0") {
-        window.location.reload();
-      }
+      findTimeToReset();
     }, 1000);
 
     return () => {
@@ -58,14 +65,23 @@ const ResetTimer = () => {
 
   return (
     <p>
-      In <AnimatedValue value={hoursTens} direction="down" />
-      <AnimatedValue value={hoursOnes} direction="down" />
-      {":"}
-      <AnimatedValue value={minutesTens} direction="down" />
-      <AnimatedValue value={minutesOnes} direction="down" />
-      {":"}
-      <AnimatedValue value={secondsTens} direction="down" />
-      <AnimatedValue value={secondsOnes} direction="down" />
+      {hoursTens &&
+        hoursOnes &&
+        minutesTens &&
+        minutesOnes &&
+        secondsTens &&
+        secondsOnes && (
+          <>
+            In <AnimatedValue value={hoursTens} direction="down" />
+            <AnimatedValue value={hoursOnes} direction="down" />
+            {":"}
+            <AnimatedValue value={minutesTens} direction="down" />
+            <AnimatedValue value={minutesOnes} direction="down" />
+            {":"}
+            <AnimatedValue value={secondsTens} direction="down" />
+            <AnimatedValue value={secondsOnes} direction="down" />
+          </>
+        )}
     </p>
   );
 };
