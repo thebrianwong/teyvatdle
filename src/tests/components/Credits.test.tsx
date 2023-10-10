2378bbf112e168b596e07db0712fa120a27344b9
Brian Wong <brianw7218@gmail.com>
2023-10-09 21:21:02 -0700

    Add test for Credits

---
+++ src/tests/components/Credits.test.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Credits from "../../components/Credits/Credits";
+
+test("Credits renders", () => {
+  render(<Credits />);
+  const component = screen.getByRole("button", { name: "Credits" });
+  expect(component).toBeInTheDocument();
+});
+
+test("Clicking the Credits button displays the Credits modal", () => {
+  render(<Credits />);
+  const button = screen.getByRole("button", { name: "Credits" });
+  act(() => {
+    userEvent.click(button);
+  });
+  const modalTitle = screen.getByRole("heading", { name: "Credits" });
+  const closeButton = screen.getByRole("button", { name: "X" });
+  expect(modalTitle).toBeInTheDocument();
+  expect(closeButton).toBeInTheDocument();
+});
+
+test("Clicking the close button closes the Credits modal", () => {
+  render(<Credits />);
+  const button = screen.getByRole("button", { name: "Credits" });
+  act(() => {
+    userEvent.click(button);
+  });
+  const closeButton = screen.getByRole("button", { name: "X" });
+  act(() => {
+    userEvent.click(closeButton);
+  });
+  const modalTitle = screen.queryByRole("heading", { name: "Credits" });
+  expect(modalTitle).toBeNull();
+});
