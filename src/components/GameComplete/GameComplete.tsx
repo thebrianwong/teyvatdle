7d893333c96c386623ae5525aab3a194b19a3e01
Brian Wong <brianw7218@gmail.com>
2023-10-21 22:43:31 -0700

    Show a summary of guesses in the completion area for character, weapon, and food

--- src/components/GameComplete/GameComplete.tsx
+++ src/components/GameComplete/GameComplete.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import paimonCheer from "../../assets/gameArea/paimonCheer.png";
 import GameCompleteProps from "./type";
 import "./styles.scss";
@@ -7,18 +7,49 @@ import determineCorrectness from "../GuessTableRow/determineCorrectness";
 
 const GameComplete = forwardRef<HTMLDivElement, GameCompleteProps>(
   ({ gameType, selectType, guesses, answer }, ref) => {
-    const displayEmojiGuesses = () => {
-      let string = "";
+    const [emojiHeaders, setEmojiHeaders] = useState<string[]>();
+    const [emojiGuesses, setEmojiGuesses] = useState<string[]>();
+
+    const chooseEmojiHeaders = () => {
+      let headerStrings: string[];
+      if (selectType === "character") {
+        headerStrings = [
+          "ㅤ♂️✨🔥📊🌸😈ㅤ",
+          "🧑📏🌐⚔️💪👾📖🎂",
+          "---------------",
+        ];
+      } else if (selectType === "weapon") {
+        headerStrings = ["ㅤ✨📊😈ㅤ", "🗡️⚔️💪👾🎰", "---------"];
+      } else if (selectType === "food") {
+        headerStrings = ["ㅤ✨ㅤㅤㅤㅤㅤ", "🍽️🍴🎀🏪✍️🎊", "-----------"];
+      }
+      setEmojiHeaders(headerStrings!);
+    };
+
+    const calculateEmojiGuesses = () => {
+      const guessStrings: string[] = [];
       guesses.forEach((guess) => {
-        const acc = determineCorrectness(selectType, guess, answer);
-        Object.keys(acc!).forEach((key) => {
-          const value = acc![key as keyof typeof acc];
+        const accuracyObject = determineCorrectness(selectType, guess, answer);
+        let guessRow = "";
+        Object.keys(accuracyObject!).forEach((key) => {
+          const value = accuracyObject![key as keyof typeof accuracyObject];
           if (value === "wrong") {
-            string = string + "🟥";
+            guessRow = guessRow + "🟥";
+          } else if (value === "partial") {
+            guessRow = guessRow + "🟨";
+          } else if (value === "correct") {
+            guessRow = guessRow + "🟩";
           }
         });
+        guessStrings.push(guessRow);
       });
+      setEmojiGuesses(guessStrings);
     };
+
+    useEffect(() => {
+      chooseEmojiHeaders();
+      calculateEmojiGuesses();
+    }, []);
 
     return (
       <div ref={ref} className="game-complete-container">
@@ -28,6 +59,24 @@ const GameComplete = forwardRef<HTMLDivElement, GameCompleteProps>(
           guess!
         </p>
         <ResetTimer />
+        {emojiHeaders && emojiGuesses && (
+          <div className="emoji-container">
+            {emojiHeaders.map((header) => {
+              return (
+                <p className="results-emojis" key={header}>
+                  {header}
+                </p>
+              );
+            })}
+            {emojiGuesses.map((guess, index) => {
+              return (
+                <p className="results-emojis" key={`${guess}-${index}`}>
+                  {guess}
+                </p>
+              );
+            })}
+          </div>
+        )}
         <img
           src={paimonCheer}
           alt="A cheering and excited Paimon."
