7ee8f4a3b1d8a386115861358a0e574b6ac08788
Brian Wong <brianw7218@gmail.com>
2023-10-24 22:22:43 -0700

    Adjsut spacing and add line between timer and summary

--- src/components/GameComplete/GameComplete.tsx
+++ src/components/GameComplete/GameComplete.tsx
@@ -8,11 +8,7 @@ import GuessesSummary from "../GuessesSummary/GuessesSummary";
 const GameComplete = forwardRef<HTMLDivElement, GameCompleteProps>(
   ({ gameType, selectType, guesses, answer }, ref) => {
     return (
-      <div
-        ref={ref}
-        style={{ scrollMarginTop: "135px" }}
-        className="game-complete-container"
-      >
+      <div ref={ref} className="game-complete-container">
         <h1>Nice Job, Traveler!</h1>
         <p>
           Come back tomorrow and Paimon will have a new {gameType} for you to
