4ed999dbdd241a70bec5ee7e5c4af756895bd6bc
Brian Wong <brianw7218@gmail.com>
2023-11-01 22:29:15 -0700

    Add PWA boilerplate from CRA template and change PWA configurations

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,3 +15,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.unregister();
