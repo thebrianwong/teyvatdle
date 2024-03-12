eb76706d08d2aad6e0b87676641516cd99ab1f10
Brian Wong <brianw7218@gmail.com>
2024-03-11 22:30:05 -0700

    Add more GraphQL dev dependencies and files

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  // schema: `${process.env.REACT_APP_BACKEND_DOMAIN}/graphql`,
+  schema: `http://localhost:4000/graphql`,
+  documents: ["src/**/*.{ts,tsx}"],
+  generates: {
+    "./src/__generated__/": {
+      preset: "client",
+      plugins: [],
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+  },
+  ignoreNoDocuments: true,
+};
+
+export default config;
