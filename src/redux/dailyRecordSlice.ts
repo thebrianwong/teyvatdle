6f7fd95ac194f1a70abf84f18d7226f54c305c28
Brian Wong <brianw7218@gmail.com>
2024-03-13 21:47:22 -0700

    Update Redux store, reducers, and selector functions for the daily record slice to match the GraphQL types
    The store has commented out keys/values that might not be needed

--- src/redux/dailyRecordSlice.ts
+++ src/redux/dailyRecordSlice.ts
@@ -1,19 +1,99 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "./store";
 import DailyRecordAPIData from "../types/data/dailyRecordAPIData.type";
+import {
+  CharacterData,
+  ConstellationData,
+  DailyRecordData,
+  FoodData,
+  FoodType,
+  Gender,
+  GenshinElement,
+  Height,
+  Region,
+  Stat,
+  TalentData,
+  TalentType,
+  WeaponData,
+  WeaponType,
+} from "../__generated__/graphql";
 
-const initialState: DailyRecordAPIData = {
-  daily_record_id: 0,
-  character_id: 0,
-  character_solved: 0,
-  weapon_id: 0,
-  weapon_solved: 0,
-  talent_id: 0,
-  talent_solved: 0,
-  constellation_id: 0,
-  constellation_solved: 0,
-  food_id: 0,
-  food_solved: 0,
+const initialState: DailyRecordData = {
+  dailyRecordId: "",
+  character: {
+    characterId: "",
+    characterName: "",
+    // gender: Gender.Male,
+    // height: Height.Medium,
+    // rarity: 0,
+    // region: Region.Mondstadt,
+    // element: GenshinElement.Anemo,
+    // weaponType: WeaponType.Sword,
+    // ascensionStat: Stat.Atk,
+    // birthday: 0,
+    // characterImageUrl: "",
+    // characterCorrectImageUrl: "",
+    // characterWrongImageUrl: "",
+    // localSpecialty: "",
+    // localSpecialtyImageUrl: "",
+    // enhancementMaterial: "",
+    // enhancementMaterialImageUrl: "",
+    // ascensionBossMaterial: "",
+    // ascensionBossMaterialImageUrl: "",
+    // talentBossMaterial: "",
+    // talentBossMaterialImageUrl: "",
+    // talentBook: [],
+    // talentBookImageUrl: [],
+    // talents: [],
+    // constellations: [],
+  },
+  characterSolved: 0,
+  weapon: {
+    // weaponId: "",
+    // weaponName: "",
+    // rarity: 0,
+    // weaponType: WeaponType.Sword,
+    // subStat: Stat.Atk,
+    // weaponImageUrl: "",
+    // weaponDomainMaterial: "",
+    // weaponDomainMaterialImageUrl: "",
+    // eliteEnemyMaterial: "",
+    // eliteEnemyMaterialImageUrl: "",
+    // commonEnemyMaterial: "",
+    // commonEnemyMaterialImageUrl: "",
+    // gacha: true,
+  },
+  weaponSolved: 0,
+  talent: {
+    talentId: "",
+    talentName: "",
+    // talentType: TalentType.NormalAttack,
+    // talentImageUrl: "",
+    // characterName: "",
+    // characterImageUrl: "",
+  },
+  talentSolved: 0,
+  constellation: {
+    constellationId: "",
+    constellationName: "",
+    // constellationLevel: 0,
+    // constellationImageUrl: "",
+    // characterName: "",
+    // characterImageUrl: "",
+  },
+  constellationSolved: 0,
+  food: {
+    foodId: "",
+    foodName: "",
+    // rarity: 0,
+    // foodType: FoodType.AtkBoostingDishes,
+    // specialDish: true,
+    // purchasable: true,
+    // recipe: true,
+    // event: true,
+    // foodImageUrl: "",
+  },
+  foodSolved: 0,
 };
 
 export const dailyRecordSlice = createSlice({
@@ -22,24 +102,24 @@ export const dailyRecordSlice = createSlice({
   reducers: {
     insertDailyRecordFromAPI: (
       state,
-      action: PayloadAction<DailyRecordAPIData>
+      action: PayloadAction<DailyRecordData>
     ) => {
       return action.payload;
     },
     updateCharacterSolvedValue: (state, action: PayloadAction<number>) => {
-      state.character_solved = action.payload;
+      state.characterSolved = action.payload;
     },
     updateWeaponSolvedValue: (state, action: PayloadAction<number>) => {
-      state.weapon_solved = action.payload;
+      state.weaponSolved = action.payload;
     },
     updateFoodSolvedValue: (state, action: PayloadAction<number>) => {
-      state.food_solved = action.payload;
+      state.foodSolved = action.payload;
     },
     updateTalentSolvedValue: (state, action: PayloadAction<number>) => {
-      state.talent_solved = action.payload;
+      state.talentSolved = action.payload;
     },
     updateConstellationSolvedValue: (state, action: PayloadAction<number>) => {
-      state.constellation_solved = action.payload;
+      state.constellationSolved = action.payload;
     },
   },
 });
@@ -54,26 +134,23 @@ export const {
 } = dailyRecordSlice.actions;
 
 export const selectDailyRecordID = (state: RootState) =>
-  state.dailyRecord.daily_record_id;
-export const selectDailyCharacterID = (state: RootState) =>
-  state.dailyRecord.character_id;
+  state.dailyRecord.dailyRecordId;
+export const selectDailyCharacter = (state: RootState) =>
+  state.dailyRecord.character;
 export const selectDailyCharacterSolved = (state: RootState) =>
-  state.dailyRecord.character_solved;
-export const selectDailyWeaponID = (state: RootState) =>
-  state.dailyRecord.weapon_id;
+  state.dailyRecord.characterSolved;
+export const selectDailyWeapon = (state: RootState) => state.dailyRecord.weapon;
 export const selectDailyWeaponSolved = (state: RootState) =>
-  state.dailyRecord.weapon_solved;
-export const selectDailyFoodID = (state: RootState) =>
-  state.dailyRecord.food_id;
+  state.dailyRecord.weaponSolved;
+export const selectDailyFood = (state: RootState) => state.dailyRecord.food;
 export const selectDailyFoodSolved = (state: RootState) =>
-  state.dailyRecord.food_solved;
-export const selectDailyTalentID = (state: RootState) =>
-  state.dailyRecord.talent_id;
+  state.dailyRecord.foodSolved;
+export const selectDailyTalent = (state: RootState) => state.dailyRecord.talent;
 export const selectDailyTalentSolved = (state: RootState) =>
-  state.dailyRecord.talent_solved;
-export const selectDailyConstellationID = (state: RootState) =>
-  state.dailyRecord.constellation_id;
+  state.dailyRecord.talentSolved;
+export const selectDailyConstellation = (state: RootState) =>
+  state.dailyRecord.constellation;
 export const selectDailyConstellationSolved = (state: RootState) =>
-  state.dailyRecord.constellation_solved;
+  state.dailyRecord.constellationSolved;
 
 export default dailyRecordSlice.reducer;
