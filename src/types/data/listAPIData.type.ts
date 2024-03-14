22c2ecbc22b12c5f64d3858a2df5a9bc6807982b
Brian Wong <brianw7218@gmail.com>
2024-03-13 21:37:57 -0700

    Change the list and table data types to refer to the generated GraphQL types

--- src/types/data/listAPIData.type.ts
+++ src/types/data/listAPIData.type.ts
@@ -1,6 +1,8 @@
+import { ConstellationData, TalentData } from "../../__generated__/graphql";
 import ConstellationAPIData from "./constellationAPIData.type";
 import TalentAPIData from "./talentAPIData.type";
 
-type ListAPIData = TalentAPIData | ConstellationAPIData;
+// type ListAPIData = TalentAPIData | ConstellationAPIData;
+type ListAPIData = TalentData | ConstellationData;
 
 export default ListAPIData;
--- src/types/data/tableAPIData.type.ts
+++ src/types/data/tableAPIData.type.ts
@@ -1,7 +1,13 @@
+import {
+  CharacterData,
+  FoodData,
+  WeaponData,
+} from "../../__generated__/graphql";
 import CharacterAPIData from "./characterAPIData.type";
 import FoodAPIData from "./foodAPIData.type";
 import WeaponAPIData from "./weaponAPIData.type";
 
-type TableAPIData = CharacterAPIData | WeaponAPIData | FoodAPIData;
+// type TableAPIData = CharacterAPIData | WeaponAPIData | FoodAPIData;
+type TableAPIData = CharacterData | WeaponData | FoodData;
 
 export default TableAPIData;
