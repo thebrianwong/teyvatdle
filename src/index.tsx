8762280e92053301987a5e599e3c8eecea85017d
Brian Wong <brianw7218@gmail.com>
2024-03-13 21:37:47 -0700

    Add Apollo Client to the root hierarchy

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,24 @@ import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const apolloClient = new ApolloClient({
+  uri: `${process.env.REACT_APP_BACKEND_DOMAIN}/graphql`,
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={apolloClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
