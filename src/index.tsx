b74b0152265f011019038a7cb85651d2f43bfc78
Brian Wong <brianw7218@gmail.com>
2024-03-16 21:19:02 -0700

    Configure Apollo Client to be able to make subscriptions

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,45 @@ import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const apolloClient = new ApolloClient({
+const httpLink = new HttpLink({
   uri: `${process.env.REACT_APP_BACKEND_DOMAIN}/graphql`,
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: process.env.REACT_APP_BACKEND_WEBSOCKET!,
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const apolloClient = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
