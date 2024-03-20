9e641927ce30d87976c4d8a84b900606176f4bed
Brian Wong <brianw7218@gmail.com>
2024-03-19 21:27:01 -0700

    Add mocks to Apollo test provider to fix App tests

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -25,6 +25,8 @@ import {
 } from "../__generated__/graphql";
 import { MockedProvider } from "@apollo/client/testing";
 import { UPDATE_DAILY_RECORD } from "../graphql/mutations/updateDailyRecord";
+import { GET_TEYVATDLE_API_DATA } from "../graphql/queries/getTeyvatdleApiData";
+import { LISTEN_FOR_DAILY_RECORD_UPDATES } from "../graphql/subscriptions/listenForDailyRecordUpdates";
 
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as initialState, store.
@@ -117,7 +119,7 @@ export function renderWithProviders(
         ],
       },
       dailyRecord: {
-        dailyRecordId: "1",
+        dailyRecordId: "0",
         character: {
           characterId: "1",
           characterName: "Paimon",
@@ -216,6 +218,122 @@ export function renderWithProviders(
         result: {
           data: {
             updateDailyRecord: { updateDailyRecord: "mock" },
+          },
+        },
+      },
+      {
+        request: {
+          query: UPDATE_DAILY_RECORD,
+          variables: {
+            id: "0",
+            type: GameDataType.Talent,
+          },
+        },
+        result: {
+          data: {
+            updateDailyRecord: { updateDailyRecord: "mock" },
+          },
+        },
+      },
+      {
+        request: {
+          query: GET_TEYVATDLE_API_DATA,
+        },
+        result: {
+          data: {
+            dailyRecordData: {
+              dailyRecordId: "1",
+              character: {
+                characterId: "1",
+                characterName: "Paimon",
+                gender: Gender.Female,
+                height: Height.Short,
+                rarity: 5,
+                region: Region.Mondstadt,
+                element: GenshinElement.Anemo,
+                weaponType: WeaponType.Catalyst,
+                ascensionStat: Stat.Atk,
+                birthday: null,
+                characterImageUrl: "dummy",
+                characterCorrectImageUrl: "Correct!",
+                characterWrongImageUrl: "Wrong...",
+                localSpecialty: "dummy",
+                localSpecialtyImageUrl: "dummy",
+                enhancementMaterial: "dummy",
+                enhancementMaterialImageUrl: "dummy",
+                ascensionBossMaterial: "dummy",
+                ascensionBossMaterialImageUrl: "dummy",
+                talentBossMaterial: "dummy",
+                talentBossMaterialImageUrl: "dummy",
+                talentBook: ["dummy"],
+                talentBookImageUrl: ["dummy"],
+              },
+              characterSolved: 0,
+              weapon: {
+                weaponId: "1",
+                weaponName: "Cubes",
+                rarity: 5,
+                weaponType: WeaponType.Catalyst,
+                subStat: Stat.Atk,
+                weaponImageUrl: "dummy",
+                weaponDomainMaterial: "dummy",
+                weaponDomainMaterialImageUrl: "dummy",
+                eliteEnemyMaterial: "dummy",
+                eliteEnemyMaterialImageUrl: "dummy",
+                commonEnemyMaterial: "dummy",
+                commonEnemyMaterialImageUrl: "dummy",
+                gacha: true,
+              },
+              weaponSolved: 0,
+              talent: {
+                talentId: "1",
+                talentName: "Eat",
+                talentType: TalentType.NormalAttack,
+                talentImageUrl: "dummy talent",
+                characterName: "Paimon",
+                characterImageUrl: "dummy",
+              },
+              talentSolved: 0,
+              constellation: {
+                constellationId: "1",
+                constellationName: "Sleep",
+                constellationLevel: 1,
+                constellationImageUrl: "dummy constellation",
+                characterName: "Paimon",
+                characterImageUrl: "dummy",
+              },
+              constellationSolved: 0,
+              food: {
+                foodId: "1",
+                foodName: "Paimon (Emergency Food)",
+                rarity: 5,
+                foodType: FoodType.AdventurersDishes,
+                specialDish: false,
+                purchasable: false,
+                recipe: false,
+                event: true,
+                foodImageUrl: "dummy",
+              },
+              foodSolved: 0,
+            },
+            characterData: [],
+            weaponData: [],
+            foodData: [],
+            talentData: [],
+            constellationData: [],
+          },
+        },
+      },
+      {
+        request: {
+          query: LISTEN_FOR_DAILY_RECORD_UPDATES,
+        },
+        result: {
+          data: {
+            dailyRecordUpdated: {
+              type: GameDataType.Character,
+              newSolvedValue: 1,
+            },
           },
         },
       },
