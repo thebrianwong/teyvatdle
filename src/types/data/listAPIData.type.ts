b7a92b0d3b048ce3646828ff652c4701c8ad7d6f
Brian Wong <brianw7218@gmail.com>
2024-03-19 22:09:59 -0700

    Remove forgotten, unneeded code

--- src/types/data/listAPIData.type.ts
+++ src/types/data/listAPIData.type.ts
@@ -1,8 +1,5 @@
 import { ConstellationData, TalentData } from "../../__generated__/graphql";
-import ConstellationAPIData from "./constellationAPIData.type";
-import TalentAPIData from "./talentAPIData.type";
 
-// type ListAPIData = TalentAPIData | ConstellationAPIData;
 type ListAPIData = TalentData | ConstellationData;
 
 export default ListAPIData;
--- src/types/data/tableAPIData.type.ts
+++ src/types/data/tableAPIData.type.ts
@@ -3,11 +3,7 @@ import {
   FoodData,
   WeaponData,
 } from "../../__generated__/graphql";
-import CharacterAPIData from "./characterAPIData.type";
-import FoodAPIData from "./foodAPIData.type";
-import WeaponAPIData from "./weaponAPIData.type";
 
-// type TableAPIData = CharacterAPIData | WeaponAPIData | FoodAPIData;
 type TableAPIData = CharacterData | WeaponData | FoodData;
 
 export default TableAPIData;
